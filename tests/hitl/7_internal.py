aee6f0c7e468aed45c96230b6fc2ad0d3554aa43
mmoo758 <yccao94@gmail.com>
2026-02-24 18:32:03 +0800

    Change fan to use pure pwm (commaai#2267)

--- tests/hitl/7_internal.py
+++ tests/hitl/7_internal.py
@@ -8,7 +8,9 @@ pytestmark = [
 ]
 
 @pytest.mark.timeout(2*60)
-def test_fan_controller(p):
+def test_fan_curve(p):
+  # ensure fan curve is (roughly) linear
+
   start_health = p.health()
 
   for power in (30, 50, 80, 100):
@@ -16,8 +18,7 @@ def test_fan_controller(p):
     while p.get_fan_rpm() > 0:
       time.sleep(0.1)
 
-    # wait until fan spins up (and recovers if needed),
-    # then wait a bit more for the RPM to converge
+    # wait until fan spins up, then wait a bit more for the RPM to converge
     p.set_fan_power(power)
     for _ in range(20):
       time.sleep(1)
@@ -26,7 +27,7 @@ def test_fan_controller(p):
     time.sleep(5)
 
     expected_rpm = Panda.MAX_FAN_RPMs[bytes(p.get_type())] * power / 100
-    assert 0.9 * expected_rpm <= p.get_fan_rpm() <= 1.1 * expected_rpm
+    assert 0.75 * expected_rpm <= p.get_fan_rpm() <= 1.25 * expected_rpm
 
   # Ensure the stall detection is tested on dos
   if p.get_type() == Panda.HW_TYPE_DOS:
@@ -49,19 +50,3 @@ def test_fan_cooldown(p):
 def test_fan_overshoot(p):
   if p.get_type() == Panda.HW_TYPE_DOS:
     pytest.skip("panda's fan controller overshoots on the comma three fans that need stall recovery")
-
-  # make sure it's stopped completely
-  p.set_fan_power(0)
-  while p.get_fan_rpm() > 0:
-    time.sleep(0.1)
-
-  # set it to 30% power to mimic going onroad
-  p.set_fan_power(30)
-  max_rpm = 0
-  for _ in range(50):
-    max_rpm = max(max_rpm, p.get_fan_rpm())
-    time.sleep(0.1)
-
-  # tolerate 10% overshoot
-  expected_rpm = Panda.MAX_FAN_RPMs[bytes(p.get_type())] * 30 / 100
-  assert max_rpm <= 1.1 * expected_rpm, f"Fan overshoot: {(max_rpm / expected_rpm * 100) - 100:.1f}%"
