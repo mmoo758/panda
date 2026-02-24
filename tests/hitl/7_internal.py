970066eb671818e802bd925c630f96a309747bdd
mmoo758 <yccao94@gmail.com>
2026-02-24 22:17:27 +0800

    sync

--- tests/hitl/7_internal.py
+++ tests/hitl/7_internal.py
@@ -10,7 +10,8 @@ pytestmark = [
 MAX_RPM = 5000
 
 @pytest.mark.timeout(2*60)
-def test_fan_controller(p):
+def test_fan_curve(p):
+  # ensure fan curve is (roughly) linear
   start_health = p.health()
 
   rpms = []
@@ -46,23 +47,3 @@ def test_fan_cooldown(p):
   for _ in range(5):
     assert p.get_fan_rpm() <= MAX_RPM*2
     time.sleep(0.5)
-
-def test_fan_overshoot(p):
-  if p.get_type() == Panda.HW_TYPE_DOS:
-    pytest.skip("panda's fan controller overshoots on the comma three fans that need stall recovery")
-
-  # make sure it's stopped completely
-  p.set_fan_power(0)
-  while p.get_fan_rpm() > 0:
-    time.sleep(0.1)
-
-  # set it to 30% power to mimic going onroad
-  p.set_fan_power(30)
-  max_rpm = 0
-  for _ in range(50):
-    max_rpm = max(max_rpm, p.get_fan_rpm())
-    time.sleep(0.1)
-
-  # tolerate 10% overshoot
-  expected_rpm = MAX_RPM*2 * 30 / 100
-  assert max_rpm <= 1.1 * expected_rpm, f"Fan overshoot: {(max_rpm / expected_rpm * 100) - 100:.1f}%"
