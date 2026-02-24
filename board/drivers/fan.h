43eacc9a1fa711462912c0b52af8a97271af15d4
mmoo758 <yccao94@gmail.com>
2026-02-24 22:52:33 +0800

    bump

--- board/drivers/fan.h
+++ board/drivers/fan.h
@@ -30,36 +30,10 @@ void fan_tick(void) {
     fan_state.tach_counter = 0U;
     fan_state.rpm = (fan_rpm_fast + (3U * fan_state.rpm)) / 4U;
 
-    // Stall detection
-    bool fan_stalled = false;
-    if (current_board->fan_stall_recovery) {
-      if (fan_state.power > 0U) {
-        if (fan_rpm_fast == 0U) {
-          fan_state.stall_counter = MIN(fan_state.stall_counter + 1U, 254U);
-        } else {
-          fan_state.stall_counter = 0U;
-        }
-
-        if (fan_state.stall_counter > (fan_state.stall_threshold*FAN_TICK_FREQ)) {
-          fan_stalled = true;
-          fan_state.stall_counter = 0U;
-          fan_state.stall_threshold = CLAMP(fan_state.stall_threshold + 2U, FAN_STALL_THRESHOLD_MIN, FAN_STALL_THRESHOLD_MAX);
-          fan_state.total_stall_count += 1U;
-
-          // datasheet gives this range as the minimum startup duty
-          fan_state.error_integral = CLAMP(fan_state.error_integral, 20.0f, 45.0f);
-        }
-      } else {
-        fan_state.stall_counter = 0U;
-        fan_state.stall_threshold = FAN_STALL_THRESHOLD_MIN;
-      }
-    }
-
     #ifdef DEBUG_FAN
       puth(fan_state.target_rpm);
       print(" "); puth(fan_rpm_fast);
       print(" "); puth(fan_state.power);
-      print(" "); puth(fan_state.stall_counter);
       print("\n");
     #endif
 
