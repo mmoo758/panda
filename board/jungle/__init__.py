002072626ca51d4d2b66df624250dc240492d02b
mmoo758 <yccao94@gmail.com>
2026-02-24 19:38:32 +0800

    cleanup panda connect (commaai#2274)

--- board/jungle/__init__.py
+++ board/jungle/__init__.py
@@ -49,7 +49,7 @@ class PandaJungle(Panda):
 
   @classmethod
   def spi_connect(cls, serial, ignore_version=False):
-    return None, None, None, None, None
+    return None, None, None, None
 
   def flash(self, fn=None, code=None, reconnect=True):
     if not fn:
